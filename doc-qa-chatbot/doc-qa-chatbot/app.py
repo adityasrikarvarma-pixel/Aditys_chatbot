beafac21b28e31210b51817f8d0e7121a49cbabe
adityasrikarvarma-pixel <adityasrikarvarma@gmail.com>
2026-08-13 19:17:56 +0530

    Initial commit: Document Q&A chatbot

--- doc-qa-chatbot/doc-qa-chatbot/app.py
+++ doc-qa-chatbot/doc-qa-chatbot/app.py
@@ -3,7 +3,7 @@ import os
 
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_groq import ChatGroq
 
